6aac1672fbd0a192f9b0999f18b12274c53fc9c1
Avash69 <mahatoavash90@gmail.com>
2025-06-19 22:14:57 +0545

    homepage

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,19 +1,48 @@
+// import { HeroSection } from "./heroSection";
+// import { WhyChooseUsSection } from "./whyChooseUsSection";
+// import ProductsSection from "./productsSection";
+// import { useLocation } from "react-router-dom";
+// import FooterSection from "../../components/footerSection";
+
+// const Homepage = () => {
+//   const location = useLocation();
+
+//   return (
+//     <>
+//       <HeroSection />
+//       <WhyChooseUsSection />
+//       <ProductsSection />
+//       <FooterSection />
+//     </>
+//   );
+// };
+// export default Homepage;
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
 import { HeroSection } from "./heroSection";
 import { WhyChooseUsSection } from "./whyChooseUsSection";
 import ProductsSection from "./productsSection";
-import { useLocation } from "react-router-dom";
 import FooterSection from "../../components/footerSection";
 
 const Homepage = () => {
   const location = useLocation();
 
+  // Scroll to top on route change for better UX
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [location.pathname]);
+
   return (
     <>
-      <HeroSection />
-      <WhyChooseUsSection />
-      <ProductsSection />
+      <main>
+        <HeroSection />
+        <WhyChooseUsSection />
+        <ProductsSection />
+      </main>
       <FooterSection />
     </>
   );
 };
+
 export default Homepage;
